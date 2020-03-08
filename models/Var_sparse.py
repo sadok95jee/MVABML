9b92bf2b73d4e11ffb352d3779de92211a2ad0eb
sadok95jee <47359533+sadok95jee@users.noreply.github.com>
2020-03-08 12:23:39 +0100

    Create Var_sparse.py

---
+++ models/Var_sparse.py
@@ -0,0 +1,19 @@
+from gpytorch.models import ApproximateGP
+from gpytorch.variational import CholeskyVariationalDistribution
+from gpytorch.variational import VariationalStrategy
+
+class GPModel(ApproximateGP):
+    def __init__(self, inducing_points):
+        if (inducing_points.ndim ==2):
+            dims = inducing_points.shape[1]
+        else:
+            dims = 1
+        variational_distribution = CholeskyVariationalDistribution(inducing_points.size(0))
+        variational_strategy = VariationalStrategy(self, inducing_points, variational_distribution, learn_inducing_locations=True)
+        super(GPModel, self).__init__(variational_strategy)
+        self.mean_module = gpytorch.means.ZeroMean()
+        self.covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel(ard_num_dims = dims))
+    def forward(self, x):
+        mean_x = self.mean_module(x)
+        covar_x = self.covar_module(x)
+        return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
